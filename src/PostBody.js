fb6660a84d8cf68b882070ffec0983a3989d5589
BeijiYang <langyuyang@outlook.com>
2017-08-21 10:59:24 +0800

    add like

--- src/PostBody.js
+++ src/PostBody.js
@@ -4,17 +4,30 @@ import './post-body.css'
 import { connect } from 'react-redux'
 
 class PostBody extends Component {
+
+  like = () => {
+    this.props.dispatch({type: 'ADD_LIKE', 'postId': this.props.postId})
+  }
+
   render() {
     let postId = this.props.postId
     let thisComments = this.props.comments.filter(
       (item) => ( item.post === postId)
     )
+    let thisLike = this.props.posts.find(
+      (item) => ( item.id === postId)
+    )
 
     return (
       <div className="post-body">
         PostBody
-        <div className="comment-num">
-          {thisComments.length} comments
+        <div className="num-wrap">
+          <div className="like-num" onClick={ this.like }>
+            {thisLike.like} like 
+          </div>
+          <div className="comment-num">
+            {thisComments.length} comments
+          </div>
         </div>
       </div>
     )
@@ -22,7 +35,8 @@ class PostBody extends Component {
 }
 
 const mapStateToProps = (state) => ({
-  comments: state.comments
+  comments: state.comments,
+  posts: state.posts
 })
 
 export default connect(mapStateToProps)(PostBody)
--- src/redux/reducers/posts.js
+++ src/redux/reducers/posts.js
@@ -1,22 +1,33 @@
 let posts = [
     {
       'id': '0',
-      'title': 'Post0'
+      'title': 'Post0',
+      'like': 3
     },
     {
       'id': '1',
-      'title': 'Post1'
+      'title': 'Post1',
+      'like': 4
     },
     {
       'id': '2',
-      'title': 'Post2'
+      'title': 'Post2',
+      'like': 3
     }
 ]
 
 export default function postsReducer(state = posts, action) {
   switch (action.type) {
-    case 'WHATEVER':
-      return state
+    case 'ADD_LIKE':
+      let newState = state.map(
+        (item) => {
+          if(item.id === action.postId){
+            return {...item, like: item.like + 1}
+          }
+          return item
+        }
+      )
+      return newState
     default:
       return state
   }
